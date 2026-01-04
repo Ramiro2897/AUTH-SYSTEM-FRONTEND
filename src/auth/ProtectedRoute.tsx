e123562f1d907c7bdcad5e891af694d2f43cb521
Ramiro <ramirodjaltor2016@gmail.com>
2026-01-03 21:06:54 -0500

    Estado de carga

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -5,7 +5,20 @@ import type { JSX } from "react";
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const { user, loading } = useAuth();
 
-  if (loading) return null;
+  if (loading)
+  return (
+    <div className="loading-screen">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <p className="message">Espere mientras despliega hahaha xd</p>
+    </div>
+  );
 
   if (!user) return <Navigate to="/login" replace />;
 
--- src/auth/PublicRoute.tsx
+++ src/auth/PublicRoute.tsx
@@ -5,7 +5,20 @@ import type { JSX } from "react";
 const PublicRoute = ({ children }: { children: JSX.Element }) => {
   const { user, loading } = useAuth();
 
-    if (loading) return null;
+    if (loading)
+    return (
+      <div className="loading-screen">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <p className="message">Espere mientras despliega hahaha xd</p>
+    </div>
+    );
 
   // Si ya está logueado, redirige al dashboard
   if (user) return <Navigate to="/dashboard" replace />;
